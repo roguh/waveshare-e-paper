918d1a2bd1dd2f477e846290eb8c26509281527e
Felina Rivera Calzadillas <felina@felina.art>
2026-02-21 21:35:45 -0700

    update paths

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -x
-scp -r roguh_pics roguh_epd_2in13b_V3.py roguh@roguh-eyeball-2.local:src/waveshare_epaper/e-Paper/RaspberryPi_JetsonNano/python
+# scp -r roguh_pics roguh_epd_2in13b_V3.py roguh@roguh-eyeball-2.local:src/waveshare_epaper/e-Paper/RaspberryPi_JetsonNano/python
 ssh roguh@roguh-eyeball-2.local killall python3
-ssh roguh@roguh-eyeball-2.local src/waveshare_epaper/e-Paper/RaspberryPi_JetsonNano/python/roguh_epd_2in13b_V3.py "$@"
+ssh roguh@roguh-eyeball-2.local src/eink/main.py "$@"
